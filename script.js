e0d0b1d6500b81486cdff43db28c311580912281
Joey Patterson <JoeyPattersonBusiness@gmail.com>
2022-08-03 12:01:30 -0400

    create playable function that takes inputs and compares to computer; modified computer choice function

--- script.js
+++ script.js
@@ -2,12 +2,35 @@
 function getComputerChoice() {
 let num = Math.floor(Math.random()* 3);
 if (num == 0) {
-    console.log("rock");
+    return "rock";
 } else if (num == 1) {
-    console.log("paper");
+    return "paper";
 } else {
-    console.log("scissors")
+    return "scissors"
+}
 }
 
+// Defined selection for computer and player to use for game.
+let lowerCase = prompt ("Choose rock, paper, or scissors").toLowerCase();
+let computerSelection = getComputerChoice()
+
+// function created allowing player to input and play against computer.
+function playRound (lowerCase, computerSelection) {
+    if (lowerCase === computerSelection) {
+        return "It's a tie!";
+    } else if(lowerCase === 'paper' && computerSelection === 'scissors') {
+        return "You lose! The computer chose scissors.";
+    } else if(lowerCase === 'scissors' && computerSelection === 'paper') {
+        return "You win! The computer chose paper.";
+    } else if(lowerCase === 'rock' && computerSelection === 'paper') {
+        return "You lose! The computer chose paper.";
+    } else if(lowerCase === 'paper' && computerSelection === 'rock') {
+        return "You win! The computer chose rock.";
+    } else if (lowerCase === 'scissors' && computerSelection === 'rock'){
+        return "You lose! The computer chose rock.";
+    } else if (lowerCase === 'rock' && computerSelection === 'scissors') {
+        return "You win! The computer chose scissors."
+    }
 }
-getComputerChoice()
+
+console.log(playRound(lowerCase, computerSelection))
