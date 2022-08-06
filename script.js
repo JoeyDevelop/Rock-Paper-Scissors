15fea3d1f45ef163f16be83e9a47978fb25df3be
Joey Patterson <JoeyPattersonBusiness@gmail.com>
2022-08-05 22:04:43 -0400

    create function to play game; edit existing function for clarity

--- script.js
+++ script.js
@@ -1,36 +1,92 @@
-// Here I created a function to randomly assign rock paper or scissors to the console.
-function getComputerChoice() {
-let num = Math.floor(Math.random()* 3);
-if (num == 0) {
-    return "rock";
-} else if (num == 1) {
-    return "paper";
-} else {
-    return "scissors"
-}
-}
-
-// Defined selection for computer and player to use for game.
-let lowerCase = prompt ("Choose rock, paper, or scissors").toLowerCase();
-let computerSelection = getComputerChoice()
-
-// function created allowing player to input and play against computer.
-function playRound (lowerCase, computerSelection) {
-    if (lowerCase === computerSelection) {
-        return "It's a tie!";
-    } else if(lowerCase === 'paper' && computerSelection === 'scissors') {
-        return "You lose! The computer chose scissors.";
-    } else if(lowerCase === 'scissors' && computerSelection === 'paper') {
-        return "You win! The computer chose paper.";
-    } else if(lowerCase === 'rock' && computerSelection === 'paper') {
-        return "You lose! The computer chose paper.";
-    } else if(lowerCase === 'paper' && computerSelection === 'rock') {
-        return "You win! The computer chose rock.";
-    } else if (lowerCase === 'scissors' && computerSelection === 'rock'){
-        return "You lose! The computer chose rock.";
-    } else if (lowerCase === 'rock' && computerSelection === 'scissors') {
-        return "You win! The computer chose scissors."
+// function to create computer choice
+function computerChoiceSelector() {
+    let num = Math.floor(Math.random() * 3);
+    if (num === 0) {
+      return "paper";
+    } else if (num === 1) {
+      return "rock";
+    } else if (num === 2) {
+      return "scissors";
     }
-}
-
-console.log(playRound(lowerCase, computerSelection))
+  }
+  
+  //let computerChoice = computerChoiceSelector();
+  
+  // create var to equal player choice
+  //let playerChoice = prompt("Choose rock, paper, or scissors.").toLowerCase();
+  
+  
+  // create function to play a round
+  /* function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+      return "It's a tie!";
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+      return "You win! The computer chose scissors.";
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+      return "You lose! The computer chose paper.";
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+      return "You win! The computer chose rock.";
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+      return "You lose! The computer chose scissors.";
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+      return "You win! The computer chose paper";
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+      return "You lose! The computer chose rock.";
+    } else {
+      return "Your answer was not a valid answer choice."
+    }
+  } */
+  
+  //playRound(playerChoice, computerChoice);
+  
+  // create function to keep score of rounds, best of 5
+  function game() {
+      let playerWins = 0;
+    let computerWins = 0;
+    let roundsPlayed = 0;
+    let gameWinner = "";
+        // create loop to repeat function
+      while (roundsPlayed < 5) {
+        roundsPlayed++;
+        let computerChoice = computerChoiceSelector();
+        let playerChoice = prompt("Choose rock, paper, or scissors.").toLowerCase();
+        console.log(playRound(playerChoice, computerChoice));
+        console.log("Player wins total:" + playerWins);
+        console.log("Computer wins total:" + computerWins);
+        
+        function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+      return "It's a tie!";
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+      playerWins++;
+      return "You win! The computer chose scissors.";
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+      computerWins++;
+      return "You lose! The computer chose paper.";
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+      playerWins++;
+      return "You win! The computer chose rock.";
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+      computerWins++;
+      return "You lose! The computer chose scissors.";
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+      playerWins++;
+      return "You win! The computer chose paper";
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+      computerWins++;
+      return "You lose! The computer chose rock.";
+    } else {
+      return "Your answer was not a valid answer choice."
+    }
+  }  
+      }
+    if (playerWins > computerWins) {
+      gameWinner = "You win!";
+    } else if (computerWins > playerWins ) {
+      gameWinner = "You lose!";
+    } else {
+      gameWinner = "You tied!";
+    }
+    console.log(gameWinner)
+  }
+  game();
